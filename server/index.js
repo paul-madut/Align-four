bbd190315242dcb970d1b538eb28df703d1ecfaa
paul-madut <paul.madut@mths.ca>
2023-03-03 01:37:39 -0500

    mongoose working

--- server/index.js
+++ server/index.js
@@ -1,23 +1,46 @@
 const express = require('express')
-const{ MongoClient } =require('mongodb');
-const app = express()
-const port = 3001
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 const userModel = require('./models/Users')
-const uri = "mongodb+srv://pmadut2003:Testdb123@nextapp.agerivi.mongodb.net/?retryWrites=true&w=majority"
 
+async function addFluffy(){
+
+  const fluffy = new userModel({ name: 'fluffy', userName:'padhduhd',email:'chdaiuyhaiku@gmail.com' });
+  await fluffy.save()
+}
+
+const app = express()
 app.use(cors())
 app.use(express.json())
 
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://pmadut2003:Testdb123@nextapp.agerivi.mongodb.net/?retryWrites=true&w=majority"
+const port = 3001
 
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected')
+})
+
+app.get('/getAllUsers', async (req,res) => {
+  try{
+    await mongoose.connect(uri,{
+       useNewUrlParser: true, 
+       useUnifiedTopology: true });
+      
+    const result = await userModel.find()
+
+    res.send(result);
+  } catch(error){
+    res.status(500).send({ error: error.message });
+
+  }finally {
+    await mongoose.disconnect();
   }
-}
-run().catch(console.dir);
+})
+
+
+app.listen(port, () => {console.log(`Server listening on port ${port}`)})
