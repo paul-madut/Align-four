328ec61d1f3e67e27669d4e7bba89a03c7623698
paul-madut <paul.madut@mths.ca>
2023-03-09 22:39:13 -0500

    Added daisy Ui

--- client/align-four/src/components/Login/Login.tsx
+++ client/align-four/src/components/Login/Login.tsx
@@ -5,13 +5,22 @@ function Login() {
     <div className='background flex justify-center h-screen w-screen bg-cover bg-[url("https://static.vecteezy.com/system/resources/previews/004/532/221/original/tic-tac-toe-seamless-background-on-dark-blue-illustration-vector.jpg")]'>
         <div className="login__container w-1/3 h-2/3 bg-white rounded-3xl shadow-2xl mt-24">
             <h1 className='text-blue-700 text-center text-4xl mt-6'>Login</h1>
-            <form>
-                <label >First name:</label>
-                <input type="text" id="fname" name="fname" value="John"/>
-                <label >Last name:</label>
-                <input type="text" id="lname" name="lname" value="Doe"/>
-                <input type="submit" value="Submit"/>
-            </form> 
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Username</span>
+              </label>
+              <label className="input-group input-group-vertical">
+                <span>Username</span>
+                <input type="text"  className="input input-bordered" />
+              </label>
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <label className="w-3/4 input-group input-group-vertical">
+                <span>password</span>
+                <input type="text"  className="input input-bordered" />
+              </label>
+              </div>
         </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -1,46 +1,66 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const userModel = require('./models/Users')
+const userModel = require("./models/Users");
 
-async function addFluffy(){
-
-  const fluffy = new userModel({ name: 'fluffy', userName:'padhduhd',email:'chdaiuyhaiku@gmail.com' });
-  await fluffy.save()
+async function addFluffy() {
+  const fluffy = new userModel({
+    name: "fluffy",
+    userName: "padhduhd",
+    email: "chdaiuyhaiku@gmail.com",
+  });
+  await fluffy.save();
 }
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const uri = "mongodb+srv://pmadut2003:Testdb123@nextapp.agerivi.mongodb.net/?retryWrites=true&w=majority"
-const port = 3001
-
-
+const uri =
+  "mongodb+srv://pmadut2003:Testdb123@nextapp.agerivi.mongodb.net/?retryWrites=true&w=majority";
+const port = 3001;
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected')
-})
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("connected");
+});
 
-app.get('/getAllUsers', async (req,res) => {
-  try{
-    await mongoose.connect(uri,{
-       useNewUrlParser: true, 
-       useUnifiedTopology: true });
-      
-    const result = await userModel.find()
+app.get("/getAllUsers", async (req, res) => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const result = await userModel.find();
 
     res.send(result);
-  } catch(error){
+  } catch (error) {
     res.status(500).send({ error: error.message });
-
-  }finally {
+  } finally {
     await mongoose.disconnect();
   }
-})
+});
 
+// app.post('/addNewUser', async (req,res) => {
+//   try{
+//     await mongoose.connect(uri,{
+//        useNewUrlParser: true,
+//        useUnifiedTopology: true });
 
-app.listen(port, () => {console.log(`Server listening on port ${port}`)})
+//     const result = await userModel.find()
+
+//     res.send(result);
+//   } catch(error){
+//     res.status(500).send({ error: error.message });
+
+//   }finally {
+//     await mongoose.disconnect();
+//   }
+// })
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
