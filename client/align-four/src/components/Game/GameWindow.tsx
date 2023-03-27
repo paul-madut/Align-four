a61d922cc8677e587a641dbcca5a3d422f131b83
paul-madut <paul.madut@mths.ca>
2023-03-27 01:18:51 -0400

    update

--- client/align-four/src/components/Game/GameWindow.tsx
+++ client/align-four/src/components/Game/GameWindow.tsx
@@ -1,15 +1,77 @@
 import React from 'react';
 
 function GameWindow() {
+
+
+
+
+    
     return (
         <div className=' grid grid-cols-7 grid-rows-1 gap-5 w-3/4 h-5/6 bg-blue-700 rounded-xl shadow-xl overflow-hidden '>
-            <div className='tile '></div>
-            <div className='tile '></div>
-            <div className='tile '></div>
-            <div className='tile '></div>
-            <div className='tile '></div>
-            <div className='tile '></div>
-            <div className='tile '></div>
+            <div className='board-col flex flex-col justify-center '>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+            </div>
+            <div className='board-col flex flex-col justify-center '>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+            </div>
+            <div className='board-col flex flex-col justify-center '>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+            </div>
+            <div className='board-col flex flex-col justify-center '>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+            </div>
+            <div className='board-col flex flex-col justify-center '>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+            </div>
+            <div className='board-col flex flex-col justify-center '>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+            </div>
+            <div className='board-col flex flex-col justify-center '>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+                <div className="tile"></div>
+            </div>
+            
             
             
         </div>
