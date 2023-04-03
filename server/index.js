258c3e0fa89f50d60cb788c3b80f9eb0a812b935
paul-madut <paul.madut@mths.ca>
2023-04-03 12:58:07 -0400

    update

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-<<<<<<< HEAD
-
-const userModel = require("./models/Users");
-
-=======
 const userModel = require("./models/Users");
 
 const app = express();
@@ -16,25 +11,11 @@ const port = 3001;
 
 main();
 
->>>>>>> Backend
 async function addFluffy() {
   const fluffy = new userModel({
     name: "fluffy",
     userName: "padhduhd",
     email: "chdaiuyhaiku@gmail.com",
-<<<<<<< HEAD
-  });
-  await fluffy.save();
-}
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-const uri =
-  "mongodb+srv://pmadut2003:Testdb123@nextapp.agerivi.mongodb.net/?retryWrites=true&w=majority";
-const port = 3001;
-=======
     score: [0, 0, 0],
   });
   await fluffy
@@ -49,7 +30,6 @@ const port = 3001;
 
 app.use(cors());
 app.use(express.json());
->>>>>>> Backend
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -57,45 +37,6 @@ db.once("open", function () {
   console.log("connected");
 });
 
-<<<<<<< HEAD
-app.get("/getAllUsers", async (req, res) => {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    const result = await userModel.find();
-
-    res.send(result);
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  } finally {
-    await mongoose.disconnect();
-  }
-});
-
-// app.post('/addNewUser', async (req,res) => {
-//   try{
-//     await mongoose.connect(uri,{
-//        useNewUrlParser: true,
-//        useUnifiedTopology: true });
-
-//     const result = await userModel.find()
-
-//     res.send(result);
-//   } catch(error){
-//     res.status(500).send({ error: error.message });
-
-//   }finally {
-//     await mongoose.disconnect();
-//   }
-// })
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
-=======
 app.get("/getAllUsers", (req, res) => {
   userModel
     .find()
@@ -120,4 +61,3 @@ async function main() {
   }).then;
   console.log("db connected");
 }
->>>>>>> Backend
