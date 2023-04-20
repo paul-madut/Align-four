3c8c0805d430a3d790aebc6a433c1b937dec5b71
paul-madut <paul.madut@mths.ca>
2023-04-19 23:38:30 -0400

    Changed from typescript to javascript

--- client/align-four/src/components/Game/GameWindow.jsx
+++ client/align-four/src/components/Game/GameWindow.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import Board from './Board';
 
-function GameWindow() {
+function GameWindow(board : Array<Array<number>>, setBoard : any, player : number, setPlayer : any) {
 
 
 
-
-    
     return (
         <div className=' grid grid-cols-7 grid-rows-1 gap-5 w-3/4 h-5/6 bg-blue-700 rounded-xl shadow-xl overflow-hidden '>
-            <div className='board-col flex flex-col justify-evenly '>
+            <div className='board-col flex flex-col justify-evenly ' onClick={board.playerMove(0,player)}>
                 <div className="tile"></div>
                 <div className="tile"></div>
                 <div className="tile"></div>
